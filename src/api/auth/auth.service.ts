eb92cfcda80cf694f6f442f72dec456037eeb449
suhrobswe <suhrobabdurazzok@gmail.com>
2025-12-25 09:41:33 +0500

    Errors corrected

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -61,7 +61,11 @@ export class AuthService {
     if (!teacher || !isMatchPass)
       throw new BadRequestException('Email or password incorrect');
 
-    const payload: IToken = { id: teacher.id, role: teacher.role };
+    const payload: IToken = {
+      id: teacher.id,
+      role: teacher.role,
+      isActive: teacher.isActive,
+    };
     const accessToken = await this.token.accessToken(payload);
     const refreshToken = await this.token.refreshToken(payload);
     await this.token.writeCookie(res, 'token', refreshToken, 30);
--- src/infrastructure/token/interface.ts
+++ src/infrastructure/token/interface.ts
@@ -1,6 +1,7 @@
 export interface IToken {
   id: string;
   role: string;
+  isActive?: boolean;
   iat?: any;
   exp?: any;
 }
